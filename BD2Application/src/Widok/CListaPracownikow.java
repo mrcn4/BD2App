682945cfdd2c33b1882328fc43b2665f920fd764
mrcn4 <itsNotAnEmail@email.com>
2013-01-23 10:46:12 +0100

    want to merge with working skills get from model

--- BD2Application/src/Widok/CListaPracownikow.java
+++ BD2Application/src/Widok/CListaPracownikow.java
@@ -1,22 +1,36 @@
 package Widok;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.Icon;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+import Komunikator.CZdarzenie;
+import Komunikator.ETypZdarzenia;
 import Model.CPracownikIT;
 
-public class CListaPracownikow extends CSzablonWidoku {
+public class CListaPracownikow extends CSzablonWidoku implements ListSelectionListener, ActionListener {
 
 	private DefaultListModel listModel;
 	private JList list;
 	private ArrayList<CPracownikIT> listaPracownikow;
+	private JButton usunButton;
+	private JButton edytujButton;
+	static final private String EDYTUJ_PRACOWNIKA = "edytuj";
+	static final private String USUN_PRACOWNIKA = "usun";
 
 	@Override
 	void ustawArgumenty(ArrayList<Object> ListaArgumentow) {
@@ -40,19 +54,66 @@ public class CListaPracownikow extends CSzablonWidoku {
 
 	@Override
 	JComponent dajKomponent() {
-		// TODO Auto-generated method stub
 		JLabel label = new JLabel("Lista pracownikow");
 		
         listModel = new DefaultListModel();
-        listModel.addElement("Koczo");
-        listModel.addElement("Korcz");
-        listModel.addElement("Mariusz");
+		Iterator<CPracownikIT> iterator = listaPracownikow.iterator();
+		while (iterator.hasNext())
+		{
+			CPracownikIT next = iterator.next();
+			listModel.addElement(next);
+		}
 
         //Create the list and put it in a scroll pane.
         list = new JList(listModel);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
-		return list;
+        list.addListSelectionListener(this);
+        
+		edytujButton = new JButton("Edytuj");
+        edytujButton.setActionCommand(EDYTUJ_PRACOWNIKA);
+        edytujButton.addActionListener(this);
+        edytujButton.setEnabled(true);
+
+        usunButton = new JButton("Usuń pracownika");
+        usunButton.setActionCommand(USUN_PRACOWNIKA);
+        usunButton.addActionListener(this);
+        usunButton.setEnabled(true);
+        
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(label);
+        mainPanel.add(list);
+        mainPanel.add(edytujButton);
+        mainPanel.add(usunButton);
+        
+		return mainPanel;
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent arg0) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String actionCommand = arg0.getActionCommand();
+		if (actionCommand == EDYTUJ_PRACOWNIKA)
+		{
+			Integer index = list.getSelectedIndex();
+			if(index != -1)
+			{
+				CPracownikIT elementAt = (CPracownikIT)list.getModel().getElementAt(index);
+				CZdarzenie zdarzenie = new CZdarzenie();
+				zdarzenie.ustawTyp(ETypZdarzenia.WYBRANO_ID_PRACOWNIKA);
+				ArrayList<Object> parametry = new ArrayList<Object>();
+				parametry.add(elementAt.dajID());
+				zdarzenie.ustawParametry(parametry);
+				setChanged();
+				notifyObservers(zdarzenie);
+			}
+		}
+		
 	}
 
 }
